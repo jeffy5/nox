fa30481d633c5a3b85014cc6df02c57fc96b20fa
Jeffrey Wu <xr-hui@qq.com>
2021-02-23 11:56:36 +0800

    feat: Remove unnecessary files.

--- app/router/router.go
+++
@@ -1,7 +0,0 @@
-skuCustomCateNS := apiV1.Group("/sku_custom_cate")
-{
-    skuCustomCateNS.POST("/index", skus.SkuCustomCateIndex)
-    skuCustomCateNS.POST("/create", skus.SkuCustomCateCreate)
-    skuCustomCateNS.POST("/update", skus.SkuCustomCateUpdate)
-    skuCustomCateNS.POST("/delete", skus.SkuCustomCateDelete)
-}
